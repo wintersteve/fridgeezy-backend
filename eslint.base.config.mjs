9ea5ed8bc87d94686846871b3882fc62b95a3d03
Steve Winter <stevewinter@outlook.de>
2026-01-08 11:53:15 +0100

    wip

--- eslint.base.config.mjs
+++ eslint.base.config.mjs
@@ -17,6 +17,10 @@ export default [
                     allow: ["^.*/eslint(\\.base)?\\.config\\.[cm]?[jt]s$"],
                     depConstraints: [
                         {
+                            sourceTag: "scope:app",
+                            onlyDependOnLibsWithTags: ["scope:shared"],
+                        },
+                        {
                             sourceTag: "scope:shared",
                             onlyDependOnLibsWithTags: ["scope:shared"],
                         },

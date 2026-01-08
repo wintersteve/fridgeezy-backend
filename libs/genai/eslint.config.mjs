a3c0139f64de7a836ca3f959266f27db06b86113
Steve Winter <stevewinter@outlook.de>
2026-01-08 12:09:46 +0100

    wip

--- libs/genai/eslint.config.mjs
+++ libs/genai/eslint.config.mjs
@@ -10,6 +10,7 @@ export default [
                 {
                     ignoredFiles: [
                         "{projectRoot}/eslint.config.{js,cjs,mjs,ts,cts,mts}",
+                        "{projectRoot}/vite.config.{js,ts,mjs,mts}",
                     ],
                 },
             ],
